9ec68532e719d073f5496e1b238a2a1d0fcffe2c
Farooq <mullafarooq4@gmail.com>
2022-04-29 19:13:15 +0530

    minor changes

--- jsPractice/weatherSite/script.js
+++ jsPractice/weatherSite/script.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
             
             $('#profile').html(`
             
-            <div class="container mx-auto">
+            <div class="container mx-auto mb-5">
             
             <div class="card" style="width: 18rem;">
             <img class="card-img-top" src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Card image cap">
@@ -23,7 +23,7 @@ $(document).ready(function(){
               <h5 class="card-title">Weather : ${data.weather[0].description}</h5>
               <p class="card-text">Actual Temperature : ${data.main.temp}&#8451</p>
               <p class="card-text">Temperature feels like : ${data.main.feels_like}&#8451</p>
-              <a href="https://www.google.com/search?q=${cityName}" class="btn btn-secondary">To learn more about this city</a>
+              <a href="https://www.google.com/search?q=${cityName}" target="_blank" class="btn btn-secondary">To learn more about this city</a>
             </div>
           </div>
             
