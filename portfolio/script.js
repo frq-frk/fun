399dd1f034260bd1d1e742a2809cd764e3fb1311
Farooq <mullafarooq4@gmail.com>
2022-05-12 19:39:34 +0530

    Portfolio Development

---
+++ portfolio/script.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+
+    $('.intro h5').fadeIn(1000, () => {
+        $('.intro h4').fadeIn(1000, () => {
+            $('.intro h2').slideDown(1000,() => {
+                $('#cv-link').fadeIn(1000);
+            });
+        })
+    });
+
+})
+
+function toggleNav(e){
+    $('#nav').toggle();
+}
+
